dbbeee83aa626822d194021d8e63558c7cfddae0
PrinceSch <sofy95@mail.ru>
2020-11-30 22:25:25 +0700

    homework 7 complete

---
+++ chat/src/server/AuthService.java
@@ -0,0 +1,10 @@
+package server;
+
+public interface AuthService {
+
+    /**
+     * @return - nickname если пользоатель есть и null если пользователя нет
+     */
+    String getNicknameByLoginAndPassword(String login, String password);
+
+}
--- chat/src/server/ClientHandler.java
+++ chat/src/server/ClientHandler.java
@@ -1,5 +1,6 @@
 package server;
 
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -10,51 +11,82 @@ public class ClientHandler {
     Server server = null;
     Socket socket = null;
     DataInputStream in;
-    DataOutputStream output;
+    DataOutputStream out;
+    private String nickname;
+
 
     public ClientHandler(Server server, Socket socket) {
         try {
             this.server = server;
             this.socket = socket;
             in = new DataInputStream(socket.getInputStream());
-            output = new DataOutputStream(socket.getOutputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while (true) {
-                            String str = in.readUTF();
+            new Thread(() -> {
+                try {
+                    // цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
 
-                            if (str.equals("/end")) {
-                                System.out.println("Клиент отключился");
+                        if (str.startsWith("/auth")) {
+                            String[] token = str.split("\\s");
+                            String newNick = server.getAuthService().getNicknameByLoginAndPassword(token[1], token[2]);
+
+                            if (newNick != null) {
+                                nickname = newNick;
+                                sendMsg("/authok " + nickname);
+                                server.subscribe(this);
+                                System.out.println("User " + nickname + " connected");
                                 break;
+                            } else {
+                                sendMsg("Incorrect login or password");
                             }
-                            server.broadCastMessage(str);
                         }
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
+                    }
+
+                    // цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.equals("/end")) {
+                            out.writeUTF("/end");
+                            break;
+                        }
+
+                        if (str.startsWith("/w")) {
+                            String[] prMsg = str.split(" ", 3);
+                            server.privateMessage(this, prMsg[1], prMsg[2]);
+                        } else {
+                            server.broadCastMsg(this, str);
+                        }
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("User disconnected");
+                    server.unsubscribe(this);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             }).start();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                this.socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    void sendMessage(String msg) {
-
+    void sendMsg(String msg) {
         try {
-            output.writeUTF(msg);
+            out.writeUTF(msg);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public String getNickname() {
+        return nickname;
     }
 }
